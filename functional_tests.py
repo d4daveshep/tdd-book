eaa7e533173f1af7b3f49c123b68147fda5c79b4
d4daveshep <david@debian-dev.daveshep.net>
2021-10-17 12:17:55 +1300

    Changes from Chapter 2

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Check out the homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Homepage title  and header mention to-do lists
+
+assert 'To-do' in browser.title
+
+browser.quit()
 
 
