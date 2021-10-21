2b8e1c8c5b4c950d91fb4e800675d2b73dcdbfb0
d4daveshep <david@debian-dev.daveshep.net>
2021-10-21 19:08:37 +1300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -31,8 +31,9 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 		
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_emements_by_tab_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table")
 		
 		# Enter a second item in text box
 				
